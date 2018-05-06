69aea825b26f1c87a3f31ad4b1fa5f0f7c511e12
Roop Pal <rpal15@simons-rock.edu>
2018-05-05 22:40:45 -0400

    rewrote parser, used premade estimator

--- supervised/main.py
+++ supervised/main.py
@@ -9,35 +9,82 @@ import pandas as pd
 def parser(filename):
     # read in data
     data = pd.read_csv(filename).values
-    Y_train = [data[i,0] for i in range(len(data)) if data[i,2] == 'Training']
-    X_train = [np.fromstring(data[i,1], dtype=np.uint8, sep=' ') for i in range(len(data)) if data[i,2] == 'Training']
-    Y_test = [data[i,0] for i in range(len(data)) if data[i,2] == 'PublicTest' or data[i,2] == 'PrivateTest']
-    X_test = [np.fromstring(data[i,1], dtype=np.uint8, sep=' ') for i in range(len(data)) if data[i,2] == 'PublicTest' or data[i,2] == 'PrivateTest']
+    train_y = [data[i,0] for i in range(len(data)) if data[i,2] == 'Training']
+    train_x = [np.fromstring(data[i,1], dtype=np.uint8, sep=' ') for i in range(len(data)) if data[i,2] == 'Training']
+    test_y = [data[i,0] for i in range(len(data)) if data[i,2] == 'PublicTest' or data[i,2] == 'PrivateTest']
+    test_x = [np.fromstring(data[i,1], dtype=np.uint8, sep=' ') for i in range(len(data)) if data[i,2] == 'PublicTest' or data[i,2] == 'PrivateTest']
 
     # whitening
     # TODO: Whiten test?
-    X_train -= np.mean(X_train, axis=0)
-    X_train /= np.std(X_train, axis=0)
+    train_x -= np.mean(train_x, axis=0)
+    train_x /= np.std(train_x, axis=0)
 
     # reshaping into 48x48x1 image
-    X_train = [i.reshape((48, 48, 1)) for i in X_train]
-    X_test = [i.reshape((48, 48, 1)) for i in X_train]
+    train_x = [i.reshape((48, 48, 1)) for i in train_x]
+    test_x = [i.reshape((48, 48, 1)) for i in train_x]
     
-    np.save('X_train', X_train)
-    np.save('Y_train', Y_train)
-    np.save('X_test', X_test)
-    np.save('Y_test', Y_test)
+    np.save('train_x', train_x)
+    np.save('train_y', train_y)
+    np.save('test_x', test_x)
+    np.save('test_y', test_y)
+
+def input_training_set():
+    train_x = np.load('./train_x.npy')
+    train_y = np.load('./train_y.npy')
+    return {'image':train_x}, train_y    
+
+def input_evaluation_set():
+    test_x = np.load('./test_x.npy')
+    test_y = np.load('./test_y.npy')
+    return {'image':test_x}, test_y    
+
+def train_input_fn(features, labels, batch_size):
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+    # Shuffle, repeat, and batch the examples.
+    return dataset.shuffle(1000).repeat().batch(batch_size)
+
+def eval_input_fn(features, labels, batch_size):
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+    # Shuffle, repeat, and batch the examples.
+    return dataset.shuffle(1000).repeat().batch(batch_size)
 
 if __name__ == '__main__':
-    # minibatch by 19
+    steps = 1
+    batch_size = 19
+    
     emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 #     parser('../fer2013.csv')
+    data = pd.read_csv('../fer2013.csv')
+    my_feature_columns = [tf.feature_column.numeric_column('image',shape=[48,48,1])]
+    # Build 2 hidden layer DNN with 10, 10 units respectively.
+    classifier = tf.estimator.DNNClassifier(
+        feature_columns=my_feature_columns,
+        # Two hidden layers of 10 nodes each.
+        hidden_units=[10, 10],
+        n_classes=7)
+    train_x,train_y = input_training_set()
+    s = time()
+    print("TRAINING")
+    classifier.train(
+        input_fn=lambda:train_input_fn(train_x, train_y, batch_size),
+        steps=steps)
+    print("DONE TRAINING:",time()-s)
     
-    X_train = np.load('./X_train.npy')
-    Y_train = np.load('./Y_train.npy')
-    X_test = np.load('./X_test.npy')
-    Y_test = np.load('./Y_test.npy')
+    test_x,test_y = input_evaluation_set()
+    eval_result = classifier.evaluate(input_fn=lambda:eval_input_fn(test_x, test_y, batch_size))
+
+    print('\nTest set accuracy: {accuracy:0.3f}\n'.format(**eval_result))
+#     train_y = [data[i,0] for i in range(len(data)) if data[i,2] == 'Training']
+#     train_x = np.load('./train_x.npy')
+#     train_y = np.load('./train_y.npy')
+#     test_x = np.load('./test_x.npy')
+#     test_y = np.load('./test_y.npy')
     
-    plt.figure(0)
-    plt.imshow(X_train[0,:,:,0], interpolation='none', cmap='gray')
-    plt.show()
+#     plt.figure(0)
+#     plt.imshow(train_x[0,:,:,0], interpolation='none', cmap='gray')
+#     plt.show()
+    
